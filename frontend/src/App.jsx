523b74c65b65730ef9dcddabcd6d84b2952b3bc5
Parth Dhavan <parthdhavan2004@gmail.com>
2024-07-01 21:50:33 +0530

    Fixes #27:Create Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,17 @@ import Nav from './components/Nav';
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        {/* <Route path="/" element={<h1>Home</h1>} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
