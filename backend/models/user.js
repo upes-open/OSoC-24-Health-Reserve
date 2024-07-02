764aa7088030fa6dfc4e446a9f2e1200e617468f
Saga690 <master.ayush.2003@gmail.com>
2024-07-02 14:51:31 +0530

    fixes

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,5 @@
 const mongoose=require('mongoose');
 
-//database
-mongoose.connect("mongodb+srv://Contributors:kj0zL1RFxCurMsm0@cluster0.mzvwlgt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 const userSchema=mongoose.Schema({
     username:String,
