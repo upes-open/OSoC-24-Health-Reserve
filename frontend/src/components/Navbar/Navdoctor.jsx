7e66f2ecb89d4251591b38dc9185cb3abd1b3cc2
Parth Dhavan <parthdhavan2004@gmail.com>
2024-07-02 15:29:39 +0530

    Fixes #17:Create navbar

---
+++ frontend/src/components/Navbar/Navdoctor.jsx
@@ -0,0 +1,27 @@
+// src/components/Navbar/Navpatient.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navdoctor.css';
+//import logo from './path/to/logo'; // Update the path to the actual location of your logo
+
+function Navdoctor() {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="/dummylogo.jpg" alt="Logo" />
+        <h1>Your Organization Name</h1>
+      </div>
+      <ul id="doc">
+        <li><Link to="/contact">CONTACT US</Link></li>
+        <li><Link to="/doctors">SEE PATIENTS</Link></li>
+      </ul>
+      <div className="profile">
+        <img src="/profile.jpeg" alt="Profile Picture" />
+        <div className="upload-text">Upload Your Image</div>
+        <span>Username</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navdoctor;
