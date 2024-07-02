5b4852bf974c01c9aa78f2f555186ba3539d7357
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2024-07-02 20:29:55 +0530

    made changes for implementing footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,45 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Login from './components/Login-page/Login';
 import Register from './components/Register-page/Register';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   const location = useLocation();
+  const [showFooter, setShowFooter] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const footer = document.querySelector('.footer');
+      if (!footer) return;
+
+      const scrollPosition = window.innerHeight + window.scrollY;
+      const footerOffset = footer.offsetTop;
+
+      if (scrollPosition >= footerOffset) {
+        setShowFooter(true);
+      } else {
+        setShowFooter(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <div className="App">
       {location.pathname !== '/' && <Nav />}
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path='/' element={<Home />} />
       </Routes>
+      { <Footer />}
     </div>
   );
 }
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,79 @@
+  // eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Footer.css';
+import twitter from './../../assets/images/twitter-logo.png';
+import linkedin from './../../assets/images/linkedin-logo.png';
+import insta from './../../assets/images/insta-logo.png';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="sb__footer__section__padding">
+        <div className="sb__footer-links">
+          <div className="sb__footer-links_div">
+            <h4>About Us</h4>
+            <a href="/">
+              <p>Home</p>
+            </a>
+            <a href="/feedback">
+              <p>Feedback</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Services</h4>
+            <a href="/lost">
+              <p>Document Upload</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Partners</h4>
+            <a href="https://upes-open.org/">
+              <p>UPES Open Community</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Feedback</h4>
+            <a href="https://upes-open.org/contact">
+              <p>Email</p>
+            </a>
+            <a href="https://upes-open.org/contact">
+              <p>Contact Number</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Social</h4>
+            <div className="socialmedia">
+              <a href="https://twitter.com/UPESDehradun/">
+                <img src={twitter} alt="twitterimage" />
+              </a>
+              <a href="https://www.facebook.com/UPESddnuk/">
+                <img src={insta} alt="fbimage" />
+              </a>
+              <a href="https://www.linkedin.com/company/open-community/">
+                <img src={linkedin} alt="linkedinimage" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="sb__footer-below">
+          <div className="sb__footer-copyright">
+            <p>
+              @{new Date().getFullYear()} CodeInn. All rights reserved.
+            </p>
+          </div>
+          <div className="sb__footer-below-links">
+            <a href="/terms">
+              <div><p>Privacy</p></div>
+            </a>
+            <a href="/terms">
+              <div><p>Security</p></div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
