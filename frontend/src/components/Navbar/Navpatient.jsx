638d85e620c1f66c6e49481e18bb80e074192f10
Vaibhav-Rawat03 <vaibhavrawat2003@gmail.com>
2024-07-03 13:36:19 +0530

    Revert "Merge pull request #54 from Saga690/main"
    This reverts commit f602d239c6ee04bbd1b0a5aa8c0c0f73c73e6fd6, reversing
    changes made to e60db926c358a323c6419c1f5c04c706259d53b6.

--- frontend/src/components/Navbar/Navpatient.jsx
+++ frontend/src/components/Navbar/Navpatient.jsx
@@ -7,22 +7,20 @@ import './Navdoctor.css';
 
 function Navpatient() {
   return (
-    <nav className='mainnav'>
-      <div className="container">
-        <div className="logo">
-          <img src="/dummylogo.jpg" alt="Logo" />
-          <h1>Your Organization Name</h1>
-        </div>
-        <ul>
-          <li><Link to="/contact">CONTACT US</Link></li>
-          <li><Link to="/doctors">SEE DOCTORS</Link></li>
-          <li><Link to="/upload">UPLOAD RECORDS</Link></li>
-        </ul>
-        <div className="profile">
-          <img src="/profile.jpeg" alt="Profile Picture" />
-          <div className="upload-text">Upload Your Image</div>
-          <span>Username</span>
-        </div>
+    <nav>
+      <div className="logo">
+        <img src="/dummylogo.jpg" alt="Logo" />
+        <h1>Your Organization Name</h1>
+      </div>
+      <ul>
+        <li><Link to="/contact">CONTACT US</Link></li>
+        <li><Link to="/doctors">SEE DOCTORS</Link></li>
+        <li><Link to="/upload">UPLOAD RECORDS</Link></li>
+      </ul>
+      <div className="profile">
+        <img src="/profile.jpeg" alt="Profile Picture" />
+        <div className="upload-text">Upload Your Image</div>
+        <span>Username</span>
       </div>
     </nav>
   );
