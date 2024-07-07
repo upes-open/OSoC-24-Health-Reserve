101ea29b7f4b8a6519f972c8ced76be47a215f84
Saga690 <master.ayush.2003@gmail.com>
2024-07-07 14:08:40 +0530

    updated user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     username: { type: String, required: true },
-    contact: { type: Number, unique:true },
-    email: { type: String, required: true, unique:true },
+    fullname: { type: String },
+    age: { type: Number },
+    gender: { type: String },
+    role: { type: String },
+    contact: { type: Number, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    license: { type: String, required: function() { return this.role === 'Doctor'; } },
+    license: { type: String, required: function () { return this.role === 'Doctor'; } },
 })
 
 module.exports = mongoose.model("user", userSchema);
