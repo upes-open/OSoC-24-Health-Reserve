6c55fa60d3e182f4dad187e300750134a106f9e5
Priyanshu Butola <priyanshubutola@gmail.com>
2024-07-14 02:55:04 +0530

    redirect issue

--- frontend/src/components/SeePatients/SeePatientCard.jsx
+++ frontend/src/components/SeePatients/SeePatientCard.jsx
@@ -1,20 +1,67 @@
-import React from 'react';
-import './SeePatientCard.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./SeePatientCard.css";
+import { Link } from "react-router-dom";
 
 const Card = ({ item }) => {
-    return (
-        <div className="Pat-Card">
-            <Link to={`/patient/${item.username}`} className="card-link">
-                <div className="info">
-                    <div className="doctor-name"><span>Patient Name: </span>{item.username}</div>
-                    {item.age && <div><span>Age: </span>{item.age}</div>}
-                    {item.gender && <div><span>Gender: </span>{item.gender}</div>}
-                    {item.contact && <div><span>Contact: </span>{item.contact}</div>}
-                </div>
-            </Link>
+  console.log(item);
+  return (
+    <div className="Pat-Card">
+      {item.role === "Doctor" && (
+        <div className="info">
+          <div className="doctor-name">
+            <span>Patient Name: </span>
+            {item.username}
+          </div>
+          {item.age && (
+            <div>
+              <span>Age: </span>
+              {item.age}
+            </div>
+          )}
+          {item.gender && (
+            <div>
+              <span>Gender: </span>
+              {item.gender}
+            </div>
+          )}
+          {item.contact && (
+            <div>
+              <span>Contact: </span>
+              {item.contact}
+            </div>
+          )}
         </div>
-    );
+      )}
+      {item.role === "Paitent" && (
+        <Link to={`/patient/${item.username}`} className="card-link">
+          <div className="info">
+            <div className="doctor-name">
+              <span>Patient Name: </span>
+              {item.username}
+            </div>
+            {item.age && (
+              <div>
+                <span>Age: </span>
+                {item.age}
+              </div>
+            )}
+            {item.gender && (
+              <div>
+                <span>Gender: </span>
+                {item.gender}
+              </div>
+            )}
+            {item.contact && (
+              <div>
+                <span>Contact: </span>
+                {item.contact}
+              </div>
+            )}
+          </div>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Card;
