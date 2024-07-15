5632064c2b02567f4d557be16be1d4cccfccd673
Saga690 <master.ayush.2003@gmail.com>
2024-07-15 14:36:57 +0530

    fixed dashboard

--- frontend/src/components/FolderDash/dash.jsx
+++ frontend/src/components/FolderDash/dash.jsx
@@ -56,23 +56,25 @@ const Try = () => {
     };
 
     return (
-        <div className="dash-container" id='box'>
-            <div className="dash">
-                <h2 className="dash-title">Online Appointment Scheduling</h2>
-                <p className="dash-description">Our medical website allows patients to schedule appointments online with ease. Choose your preferred doctor and time slot, and get instant confirmation.</p>
-            </div>
-            <div className="dash">
-                <h2 className="dash-title">24/7 Telemedicine Services</h2>
-                <p className="dash-description">Access healthcare from the comfort of your home with our 24/7 telemedicine services. Consult with healthcare professionals through video calls anytime, anywhere.</p>
-            </div>
-            <div className="dash">
-                <h2 className="dash-title">Comprehensive Health Records</h2>
-                <p className="dash-description">Our platform offers a secure and comprehensive digital health record system. Track your medical history, lab results, and prescriptions in one place.</p>
+        <div className="full-dash-container">
+            <div className="dash-container" id='box'>
+                <div className="dash">
+                    <h2 className="dash-title">Online Appointment Scheduling</h2>
+                    <p className="dash-description">Our medical website allows patients to schedule appointments online with ease. Choose your preferred doctor and time slot, and get instant confirmation.</p>
+                </div>
+                <div className="dash">
+                    <h2 className="dash-title">24/7 Telemedicine Services</h2>
+                    <p className="dash-description">Access healthcare from the comfort of your home with our 24/7 telemedicine services. Consult with healthcare professionals through video calls anytime, anywhere.</p>
+                </div>
+                <div className="dash">
+                    <h2 className="dash-title">Comprehensive Health Records</h2>
+                    <p className="dash-description">Our platform offers a secure and comprehensive digital health record system. Track your medical history, lab results, and prescriptions in one place.</p>
+                </div>
             </div>
 
             {user.role === "Patient" && <form onSubmit={handleSubmit} className="appointment-form">
-                <div className="form-group">
-                    <label htmlFor="doctor">Enter Doctor's Email:</label>
+                <div className="form-group-dash">
+                    <label htmlFor="doctor">Doctor's Email:</label>
                     <input
                         type="text"
                         id="doctor"
